a38598105bc6a982c46866e9d08c7a83907b4b2b
Dash <cs.phat.nguyen@gmail.com>
2019-11-05 00:30:41 -0800

    'unittest'

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/example2/StringReverserTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/example2/StringReverserTest.java
@@ -13,25 +13,13 @@ public class StringReverserTest {
     StringReverser SUT;
 
     @Before
-    public void setup() throws Exception {
+    public void setUp() throws Exception {
         SUT = new StringReverser();
     }
 
     @Test
-    public void reverse_emptyString_emptyStringReturned() throws Exception {
+    public void reserve_emptyString_emptyStringReturned() throws Exception{
         String result = SUT.reverse("");
         assertThat(result, is(""));
-    }
-
-    @Test
-    public void reverse_singleCharacter_sameStringReturned() throws Exception {
-        String result = SUT.reverse("a");
-        assertThat(result, is("a"));
-    }
-
-    @Test
-    public void reverse_longString_reversedStringReturned() throws Exception {
-        String result = SUT.reverse("Vasiliy Zukanov");
-        assertThat(result, is("vonakuZ yilisaV"));
     }
 }
